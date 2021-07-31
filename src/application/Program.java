4b00fb74eeaf9014fe7b353def75c4ba52cb48bc
Leônidas Rosa <leogui.moritz@gmail.com>
2021-07-31 11:08:04 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Classe criada apenas para teste de criação de repositório no GitHub");
+	}
+
+}
