9ff63737fc210452de9c633fb6e86ba161a47048
Leônidas Rosa <leogui.moritz@gmail.com>
2021-07-31 12:11:51 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Primeiro commit local -> remoto para criação do projeto
+		System.out.println("Projeto criado");
+
+	}
+
+}
